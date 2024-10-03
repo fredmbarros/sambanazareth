1978dc175b0aa9278995fb3ca4ae54d6f734e812
Fred Barros <fredmbarros@gmail.com>
2024-10-03 00:20:36 -0400

    email copied fade out

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 document.getElementById('my-button').addEventListener('click', function() {
     const email = "samba.nazareth.2024@gmail.com";
+    
     navigator.clipboard.writeText(email).catch(function(error) {
         console.error('Error copying email: ', error);
     });
-    this.style.backgroundImage = "url('assets/logo-Nazareth-VERAO.png')";
-        document.getElementById('email-copied').style.display = 'block';
-        document.getElementById('thanks').style.display = 'block';
-});
 
+    this.style.backgroundImage = "url('assets/logo-Nazareth-VERAO.png')";
+
+    const emailCopied = document.getElementById('email-copied');
+
+    emailCopied.style.display = 'block';
+    emailCopied.classList.add('show');
+
+    document.getElementById('thanks').style.display = 'block';
+    document.getElementById('protip').style.display = 'block';
+
+    setTimeout(function() {
+        emailCopied.classList.remove('show');
+        setTimeout(function() {
+            emailCopied.style.display = 'none';
+        }, 1000);
+    }, 1000);
+});
